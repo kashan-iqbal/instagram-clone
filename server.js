ba294994a5fee5f8d70a47af7e8bcdd52cddab90
kashan-iqbal <kashaniqbal33@gmail.com>
2024-01-27 11:25:58 +0500

    some changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 const connectDb = require("./config/ConnectDb");
 const cors = require("cors");
 const body_Parser = require("body-parser");
-const path = require("path");
+// const path = require("path");
 
 // load evviroment variable from .envfile
 dotenv.config();
@@ -25,14 +25,14 @@ app.use("/api/v1/user", require("./Route/UserRoutes"));
 app.use("/api/v1/post", require("./Route/CreatePost"));
 
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+// app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.use("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html")),
-  function (err) {
-    res.send(err);
-  };
-});
+// app.use("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html")),
+//   function (err) {
+//     res.send(err);
+//   };
+// });
 
 // server is listing on specitfic port
 // setup prot
